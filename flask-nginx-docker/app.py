33189c19f09038b1ff178a67a7d6a1cc52b8ddf2
MattheWhite <fehermatyas97@gmail.com>
2023-04-09 17:12:24 +0200

    Add the application source code declaration, using Flask web framework.

---
+++ flask-nginx-docker/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, You Developer Engineer Master DEVOPS</p>"
+
+
+# default port for flask : 5000
+if __name__ == "__main__":
+    app.run(
+        # host="0.0.0.0",
+        port=8888
+        )
